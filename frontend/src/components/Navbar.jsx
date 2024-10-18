e0a321766d11b2c51087b6378291647f23e182a8
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-10-18 22:05:52 +0530

    responsive navibar. added menu icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,8 @@ const Navbar = () => {
                 <hr className='border-none outline-none bg-primary m-auto w-3/5 h-0.5 hidden'/>
             </NavLink>
         </ul>
-        {
+       <div className='flex gap-4 items-center'>
+       {
             token
             ?<div className='flex items-center gap-2 cursor-pointer group relative'>
                 <img src={assets.profile_pic} alt=""  className='w-8 h-8 rounded-full'/>
@@ -44,8 +45,22 @@ const Navbar = () => {
             :<div><button onClick={()=>navigate('/login')} className='bg-primary  text-white py-2 px-5 rounded-full font-light md:block'>Create Account</button></div>
 
         }
+        <img onClick={()=>setShowMenu(true)} className='block md:hidden w-6' src={assets.menu_icon} alt="" />
+        {/*------   Mobile Menu       -----*/}
+        <div className={`${showMenu ? 'fixed w-full': 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+            <div className='flex items-center justify-between px-5 py-8'>
+                <img className='h-10' src={assets.logo} alt="" />
+                <img className='w-10' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <ul className='flex flex-col px-5 py-5 gap-8'>
+               <NavLink onClick={()=>setShowMenu(false)} to='/'><p>HOME</p></NavLink>
+               <NavLink onClick={()=>setShowMenu(false)} to='/doctors'><p>ALL DOCTORS</p></NavLink>
+               <NavLink onClick={()=>setShowMenu(false)} to='/about'><p>ABOUT</p></NavLink>
+               <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p>CONTACT</p></NavLink>
+            </ul>
+        </div>
+       </div>
        
-      
     </div>
   )
 }
