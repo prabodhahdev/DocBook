51775211ac8634808fba5011cb3a689259aaaa63
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-10-19 20:59:54 +0530

    database config

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
 
 //app config
 const app=express()
 const port=process.env.PORT || 8000
+connectDB()
 
 //middlewares
 app.use(express.json()) // when we pass the request it will send as a json format
 app.use(cors()) // this allows connect frontend to backend
+
 
 //api end point
 app.get('/',(req,res)=>{
