5a6a387fa7bb6444776aff134e6a7dc7cd9996a2
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-10-21 16:06:06 +0530

    user model

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -14,6 +14,10 @@ const doctorSchema=new mongoose.Schema({
         type:String,
         required:true
     },
+    image:{
+        type:String,
+        required:true
+    },
     speciality:{
         type:String,
         required:true
