2a3f03649fb484444b19da2100b496fe4c54d188
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-10-21 15:53:19 +0530

    doctor model and cloudinary config

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 //app config
 const app=express()
 const port=process.env.PORT || 8000
 connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json()) // when we pass the request it will send as a json format
