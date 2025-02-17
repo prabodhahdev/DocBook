8905144a076218d24d7e0626a1b536e74a05c11d
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2025-02-17 12:50:09 +0530

    doctorlist coded and changed font family to outfit

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,15 +1,38 @@
 import { createContext, useState } from "react";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const AdminContext = createContext();
 
 
 const AdminContextProvider = (props) =>{
     const [aToken , setAtoken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
+    const [doctors,setDoctors] = useState([])
+
+    const getAllDoctors = async () =>{
+        try {
+            
+            const {data} = await axios.get('http://localhost:8000/api/admin/alldoctors',{headers:{aToken}})
+            
+            if(data.success){
+                setDoctors(data.doctors)
+                console.log(data.doctors)
+            }else{
+                toast.error(data.msg)
+            }
+
+        } catch (error) {
+            toast.error(error.msg)
+        }
+    }
 
     const value ={
         aToken,
-        setAtoken 
+        setAtoken ,
+        doctors,
+        getAllDoctors
     }
+
     
     return (
         <AdminContext.Provider value={value}>
